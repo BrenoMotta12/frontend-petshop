b95a462ced022658cab07c18efe763dc7be12f87
Arilson1 <arilsontere18@hotmail.com>
2024-03-25 20:52:42 -0300

    Rotas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,22 +1,15 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./screens/Home/Home";
-import ProductRegister from "./screens/ProductRegister/ProductRegister";
-import Realeases from "./screens/Realeases/Realeases";
 import { Login } from "./screens/Login/Login";
 import NavBar from "./screens/Navbar/NavBar";
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
+            <NavBar />
             <Routes>
-               
                 <Route path="/login" element={<Login />} />
-                <Route element={<NavBar />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/productregister" element={<ProductRegister />} />
-                    <Route path="/realeases" element={<Realeases />} />
-                </Route>
+                <Route path="/*" element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,8 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import styles from './Home.module.css';
+import ProductRegister from '../ProductRegister/ProductRegister';
+import Realeases from '../Realeases/Realeases';
 
 export function Home() {
 
     return (
-        <div className={styles.home}>Home</div>
+        <div className={styles.home}>
+            <Routes>
+                <Route path="/productregister" element={<ProductRegister />} />
+                <Route path="/realeases" element={<Realeases />} />
+            </Routes>
+        </div>
     )
 }
